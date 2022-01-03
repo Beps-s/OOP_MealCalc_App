3ffc3f3c976be57a38646c2b15b1cc03ed04636b
UwUBeebi <chris.erman@khk.ee>
2022-01-03 13:49:27 +0200

    Fixed an error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const StorageCtrl = (function(){
   return {
     storeItem: function(item){
       let items;
-      if(localStorage. getItem('items') === null){
+      if(localStorage.getItem('items') === null){
         items = [];
         items.push(item);
         localStorage.setItem('items', JSON.stringify(items));
@@ -17,7 +17,7 @@ const StorageCtrl = (function(){
       if(localStorage.getItem('items') === null){
         items = [];
       } else {
-        item = JSON.parse(localStorage.getItem('items'));
+        items = JSON.parse(localStorage.getItem('items'));
       }
       return items;
     }
@@ -28,18 +28,18 @@ const ItemCtrl = (function(){
   const Item = function(id, name, calories){
     this.id = id
     this.name = name
-    this.calories = calories 
+    this.calories = calories
   }
-  
+
   const data = {
     items: [
-        //{id: 0, name: 'Steak Dinner', calories: 1200},
-        //{id: 1, name: 'Cookie', calories: 400},
-        //{id: 2, name: 'Eggs', calories: 300}
+      //{id: 0, name: 'Steak Dinner', calories: 1200},
+      //{id: 1, name: 'Cookie', calories: 400},
+      //{id: 2, name: 'Eggs', calories: 300}
     ],
     total: 0
   }
-  
+
   return {
     getItems: function(){
       return data.items
@@ -51,9 +51,9 @@ const ItemCtrl = (function(){
       } else {
         ID = 0
       }
-      calories = parseInt(calories)
-      newItem = new Item(ID, name, calories)
-      data.Items.push(newItem)
+      calories = parseInt(calories);
+      newItem = new Item(ID, name, calories);
+      data.items.push(newItem);
       return newItem
     },
     getTotalCalories: function(){
@@ -62,19 +62,19 @@ const ItemCtrl = (function(){
         total = total + item.calories;
         console.log(total)
       });
-      data.total = total
+      data.total = total;
       console.log(data.total)
       return data.total;
     },
-   logData: function(){
-        return data
-      }
+    logData: function(){
+      return data
     }
+  }
 })();
 
 const UICtrl = (function(){
   const UISelectors = {
-    itemList: "#item-list",
+    itemList: '#item-list',
     itemNameInput: '#item-name',
     itemCaloriesInput: '#item-calories',
     addBtn: '.add-btn',
@@ -84,31 +84,31 @@ const UICtrl = (function(){
     populateItemList: function(items){
       let html = '';
       items.forEach(function(item){
-        html+=`<li class="collection-item" id="item-${item.id}">
-        <strong>${item.name}: </strong><em>${item.calories}Calories</em>
+        html += `<li class="collection-item" id="item-${item.id}">
+        <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
         <a href="#" class="secondary-content">
           <i class="edit-item fa fa-pencil"></i>
         </a>
         </li>`;
-      })
+      });
       document.querySelector(UISelectors.itemList).innerHTML = html;
     },
-    getItemInput: function(){
-      return{
-        name: document.querySelector(UISelectors.itemNameInput).value,
-        calories: document.querySelector(UISelectors.itemCaloriesInput).value
-      }
-    },
     getSelectors: function(){
-      return UISelectors
+      return UISelectors;
+    },
+    getItemInput: function(){
+      return {
+        name:document.querySelector(UISelectors.itemNameInput).value,
+        calories:document.querySelector(UISelectors.itemCaloriesInput).value
+      }
     },
     addListItem: function(item){
       const li = document.createElement('li');
-      li.className = 'collection-item'
-      li.id = `item-${item.id}`;
-      li.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em> <a href="#" class="secondary-content">
-          <i class="edit-item fa fa-pencil"></i>
-        </a>`;
+      li.className = 'collection-item';
+      li.ad = `item-${item.id}`;
+      li.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories </em> <a href="#" class="secondary-content>
+      <i class="edit-item fa fa-pencil"></i>
+      </a>`;
       document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li)
     },
     clearInput: function(){
@@ -122,35 +122,35 @@ const UICtrl = (function(){
 })();
 
 const App = (function(ItemCtrl, StorageCtrl, UICtrl){
-  const loadEventListeners = function(){
-    const UISelectors = UICtrl.getSelectors()
-    document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
-    document.addEventListener('DOMContentLoaded', getItemsFromStorage)
-  }
-  const itemAddSubmit = function(event){
-    const input = UICtrl.getItemInput()
-    if(input.name !== '' && input.calories !== ''){
-      const newItem = ItemCtrl.addItem(input.name, input.calories)
-      UICtrl.addListItem(newItem)
-      const totalCalories = ItemCtrl.getTotalCalories();
-      UICtrl.showTotalCalories(totalCalories);
-      StorageCtrl.storeItem(newItem);
-      UICtrl.clearInput();
+    const loadEventListeners = function(){
+      const UISelectors = UICtrl.getSelectors();
+      document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+      document.addEventListener('DOMContentLoaded', getItemsFromStorage)
     }
-    event.preventDefault()
-  }
-  const getItemsFromStorage = function(){
-    const items = StorageCtrl.getItemsFromStorage()
-    UICtrl.populateItemList(items)
-  }
-  return {
-      init: function(){
-        console.log('Initializing App')
-        const items = ItemCtrl.getItems()
-        UICtrl.populateItemList(items)
-        loadEventListeners()
+    const itemAddSubmit = function(event){
+      const input = UICtrl.getItemInput()
+      if(input.name !== '' && input.calories !==''){
+        const newItem = ItemCtrl.addItem(input.name, input.calories)
+        UICtrl.addListItem(newItem)
+        const totalCalories = ItemCtrl.getTotalCalories();
+        UICtrl.showTotalCalories(totalCalories);
+        StorageCtrl.storeItem(newItem);
+        UICtrl.clearInput();
       }
+      event.preventDefault()
     }
-}) (ItemCtrl, StorageCtrl, UICtrl);
+    const getItemsFromStorage = function(){
+      const items = StorageCtrl.getItemsFromStorage()
+      UICtrl.populateItemList(items)
+    }
+  return {
+    init: function(){
+      console.log('Initializing App')
+      const items = ItemCtrl.getItems()
+      UICtrl.populateItemList(items)
+      loadEventListeners();
+    }
+  }
+})(ItemCtrl, StorageCtrl, UICtrl);
 
 App.init()
